266712b85cfa42b70bc5d8530286be84c93f25b5
Joffrey Bion <joffrey.bion@gmail.com>
2022-01-17 01:18:36 +0100

    Basic gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "org.hildan.dumbocr"
+description = "A dumb OCR that recognizes characters in an image given a set of base character images"
+
+repositories {
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "dumb-ocr"

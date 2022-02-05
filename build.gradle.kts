dc1a01b8fe34f806fb0e75edebeccefaa3b72843
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-05 22:47:53 +0100

    Add publication configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.6.10"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+    id("org.hildan.github.changelog") version "1.11.1"
 }
 
 group = "org.hildan.ocr"
 description = "A simple OCR that recognizes characters in an image given a set of base character images"
+
+changelog {
+    futureVersionTag = project.version.toString()
+}
 
 repositories {
     mavenCentral()
@@ -16,4 +25,69 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val dokkaJavadocJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka into a Javadoc jar"
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc)
+}
+
+nexusPublishing {
+    packageGroup.set("org.hildan")
+    repositories {
+        sonatype()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+
+            artifact(sourcesJar)
+            artifact(dokkaJavadocJar)
+
+            val githubUser = findProperty("githubUser") as String? ?: System.getenv("GITHUB_USER")
+            val githubSlug = "$githubUser/${rootProject.name}"
+            val githubRepoUrl = "https://github.com/$githubSlug"
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set(githubRepoUrl)
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("joffrey-bion")
+                        name.set("Joffrey Bion")
+                        email.set("joffrey.bion@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:$githubRepoUrl.git")
+                    developerConnection.set("scm:git:git@github.com:$githubSlug.git")
+                    url.set(githubRepoUrl)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["maven"])
 }

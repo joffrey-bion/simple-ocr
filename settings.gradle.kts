fc2fbef38d6dd7ea410dc541981a186f54dbd64d
Joffrey Bion <joffrey.bion@gmail.com>
2024-04-08 16:09:46 +0200

    Setup build scans / develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+plugins {
+    id("com.gradle.develocity") version "3.17"
+}
+
 rootProject.name = "simple-ocr"
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = false // bad for CI, and not critical for local runs
+    }
+}
